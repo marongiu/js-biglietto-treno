7d13c1b96f790e9857e216b208f6507abed6ddf5
Andrea <andreamarongiu05@gmail.com>
2021-03-01 15:58:12 +0100

    Inserimento condizioni

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,38 @@
 
 var km = prompt("Quanti chilometri percorrerai?");
 
-var eta = prompt("Quanti anni hai?");
+var eta = parseInt(prompt("Quanti anni hai?"));
 
 // Calcolo il costo del Biglietto
 
-var costo = (km * 0.21)
+var costo = (km * 0.21);
 
-console.log(costo);
+console.log(costo, eta);
+
+
+// Sconti per i passegeri
+
+// Under
+var under = (18);
+
+var underCosto = ((costo * 20) / 100 );
+
+
+// Over
+
+var over = (65);
+
+var overCosto = ((costo * 40) / 100)
+
+
+
+// Inserisco le variabili nelle condizioni
+
+
+if (eta < under) {
+  console.log(underCosto);
+} else if (eta > 65) {
+  console.log(overCosto);
+} else {
+  console.log("Non sei compreso tra queste due eta");
+}

6d239058a6726b42ce4960e89e077ea9b0b94eae
Andrea <andreamarongiu05@gmail.com>
2021-03-01 15:15:38 +0100

    Inserisco i due prompt e il costo del biglietto per km

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log("ok");
+// Creo due variabili con prompt per visualizzare età e km
+
+var km = prompt("Quanti chilometri percorrerai?");
+
+var eta = prompt("Quanti anni hai?");
+
+// Calcolo il costo del Biglietto
+
+var costo = (km * 0.21)
+
+console.log(costo);
